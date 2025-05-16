04de27a22378a27ae423a9c9d753d68881953408
Aditya Phasu <adityaphasu919@gmail.com>
2025-05-16 21:19:36 +0530

    Add links data structure for Developers, Ecosystem, Community, and Resources

---
+++ data/links.ts
@@ -0,0 +1,171 @@
+export const Developers = [
+  {
+    title: "Developers Hub",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "SN Stack",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Build with AI",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Modular Ecosystem",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Tools & Resources",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Documentation",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Cairo Book",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Tutorials",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Roadmap",
+    link: "",
+    showArrow: false,
+  },
+];
+
+export const Ecosystem = [
+  {
+    title: "Crypto Bridge",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Ecosystem Projects",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Crypto Wallets",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Bridges & on-ramps",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "DeFi dApps",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Crypto Games",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Block Explorers & Monitoring tools",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Full-nodes & RPC services",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Starknet Grants",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Starknet Status Page",
+    link: "",
+    showArrow: true,
+  },
+];
+
+export const Community = [
+  {
+    title: "Explore Starknet",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Events",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Ambassadors Program",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Jobs",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Governance",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Staking",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Community Forum",
+    link: "",
+    showArrow: true,
+  },
+  {
+    title: "Online communities",
+    link: "",
+    showArrow: false,
+  },
+];
+
+export const Resources = [
+  {
+    title: "Blog",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "FAQs",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Glossary",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Media Kit",
+    link: "",
+    showArrow: false,
+  },
+  {
+    title: "Account Abstraction",
+    link: "",
+    showArrow: false,
+  },
+];
