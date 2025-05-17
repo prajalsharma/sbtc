3eb524be66bd0ce9d72438eedb68080a501347cc
Aditya Phasu <adityaphasu919@gmail.com>
2025-05-18 00:53:15 +0530

    Refactor Header component to enhance navigation structure and improve mobile menu functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import { useState } from "react";
+import Link from "next/link";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 import Discord from "./socials/discord";
 import { X } from "./socials/x";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Menu, XIcon } from "lucide-react";
 import { Developers, HeaderCommunity, HeaderEcosystem, HeaderResources } from "@/data/links";
-import Link from "next/link";
 
 const Header = () => {
   const isDesktop = useMediaQuery("(min-width: 1024px)");
-  //   const [openMenu, setOpenMenu] = useState<string | null>(null);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openList, setOpenList] = useState<string | null>(null);
 
   return (
     <header className="bg-header-bg sticky z-[1000] top-0">
@@ -28,42 +30,56 @@ const Header = () => {
           </Link>
         </div>
         <nav className="flex flex-row items-center bg-header-bg basis-0 lg:basis-full lg:justify-between ml-auto">
-          <div>
+          <div
+            className={`fixed w-full h-screen-minus-79 z-99 top-[79px] left-0  transition-transform items-center flex flex-col bg-header-bg overflow-auto lg:overflow-visible lg:visible lg:top-0 lg:static lg:w-auto lg:h-auto ${
+              openMenu ? "translate-x-0" : "-translate-x-full lg:translate-0"
+            }`}>
             <ul className="flex w-full p-5 flex-col lg:flex-row lg:gap-4.5 lg:py-0 lg:pr-0 lg:pl-8.5">
+              <NavMenuItem
+                label="Developers"
+                list={Developers}
+                isDesktop={isDesktop}
+                openList={openList}
+                setOpenList={setOpenList}
+                submenuComponent={HeaderSubMenu}
+              />
+              <NavMenuItem
+                label="Ecosystem"
+                list={HeaderEcosystem}
+                isDesktop={isDesktop}
+                openList={openList}
+                setOpenList={setOpenList}
+                submenuComponent={HeaderSubMenu}
+              />
+              <NavMenuItem
+                label="Community"
+                list={HeaderCommunity}
+                isDesktop={isDesktop}
+                openList={openList}
+                setOpenList={setOpenList}
+                submenuComponent={HeaderSubMenu}
+              />
               <li
-                className="relative group"
-                // onClick={() => setOpenMenu(openMenu === "developers" ? null : "developers")}
-              >
-                <span className="header-list-heading">
-                  Developers
-                  <ChevronDown className="transition-transform mt-0.5 ml-2 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial" />
+                className={`relative ${isDesktop && "group"}`}
+                onClick={() => setOpenList(openList === "Resources" ? null : "Resources")}>
+                <span
+                  className={`header-list-heading rounded-sm ${
+                    !isDesktop && openList === "Resources" && "bg-[#AFCAFF] text-header-bg "
+                  }`}>
+                  Resources
+                  <ChevronDown
+                    className={`transition-transform mt-0.5 ml-2 size-8 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial ${
+                      !isDesktop && openList === "Resources" && "-rotate-180"
+                    }`}
+                  />
                 </span>
-                <HeaderSubMenu
-                  list={Developers}
-                  // isOpen={openMenu === "developers"}
+                <ResourceMenu
+                  list={HeaderResources}
+                  isOpen={openList === "Resources"}
+                  isDesktop={isDesktop}
                 />
               </li>
-              <li className="relative group">
-                <span className="header-list-heading">
-                  Ecosystem
-                  <ChevronDown className="transition-transform mt-0.5 ml-2 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial" />
-                </span>
-                <HeaderSubMenu list={HeaderEcosystem} />
-              </li>
-              <li className="relative group">
-                <span className="header-list-heading">
-                  Community
-                  <ChevronDown className="transition-transform mt-0.5 ml-2 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial" />
-                </span>
-                <HeaderSubMenu list={HeaderCommunity} />
-              </li>
-              <li className="relative group">
-                <span className="header-list-heading">
-                  Resources
-                  <ChevronDown className="transition-transform mt-0.5 ml-2 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial" />
-                </span>
-                <ResourceMenu list={HeaderResources} />
-              </li>
+              {!isDesktop && <HeaderSocials />}
             </ul>
           </div>
           <a
@@ -73,12 +89,57 @@ const Header = () => {
             Bridge
           </a>
           {isDesktop && <HeaderSocials />}
+          <button
+            aria-expanded={openMenu}
+            onClick={() => {
+              setOpenMenu(!openMenu);
+              setOpenList(null);
+            }}
+            className="lg:hidden py-3 pl-6">
+            {openMenu ? <XIcon color="white" /> : <Menu color="white" />}
+            <span className="sr-only">Menu</span>
+          </button>
         </nav>
       </div>
     </header>
   );
 };
 export default Header;
+
+type NavMenuItemProps = {
+  label: string;
+  list: any[];
+  isDesktop: boolean;
+  openList?: string | null;
+  setOpenList?: (val: string | null) => void;
+  submenuComponent?: React.ComponentType<{ list: any[]; isOpen?: boolean }>;
+};
+
+const NavMenuItem = ({
+  label,
+  list,
+  isDesktop,
+  openList,
+  setOpenList,
+  submenuComponent: SubmenuComponent = HeaderSubMenu,
+}: NavMenuItemProps) => (
+  <li
+    className={`relative ${isDesktop && "group"}`}
+    onClick={setOpenList ? () => setOpenList(openList === label ? null : label) : undefined}>
+    <span
+      className={`header-list-heading rounded-sm ${
+        !isDesktop && openList === label && "bg-[#AFCAFF] text-header-bg "
+      }`}>
+      {label}
+      <ChevronDown
+        className={`transition-transform mt-0.5 ml-2 size-8 lg:size-[13px] group-hover:-rotate-180 duration-300 ease-initial
+        ${!isDesktop && openList === label && "-rotate-180 "}
+        `}
+      />
+    </span>
+    <SubmenuComponent list={list} isOpen={openList === label} />
+  </li>
+);
 
 const HeaderSocials = () => {
   return (
@@ -110,8 +171,8 @@ const HeaderSubMenu = ({
 }) => {
   return (
     <ul
-      className={`lg:block lg:absolute header-submenu bg-[#1B1B1B] rounded-[8px] lg:p-6 lg:w-max lg:right-auto transition-all lg:left-0 opacity-0 origin-top text-left top-auto scale-95 translate-z-0 group-hover:scale-100 group-hover:opacity-100 group-hover:left-auto group-hover:delay-150 group-hover:h-auto group-hover:overflow-visible
-     ${isOpen ? "transform-none opacity-100 left-auto delay-150 h-auto overflow-visible" : ""}`}>
+      className={`lg:block lg:absolute header-submenu bg-[#1B1B1B] rounded-[8px] lg:p-6 lg:w-max lg:right-auto lg:transition-all lg:left-0 lg:opacity-0 origin-top text-left top-auto lg:scale-95 translate-z-0 group-hover:scale-100 group-hover:opacity-100 group-hover:left-auto group-hover:delay-150 w-full group-hover:overflow-visible lg:pointer-events-none lg:group-hover:pointer-events-auto overflow-hidden
+      ${isOpen ? "flex flex-col py-2 pointer-events-auto" : "hidden pointer-events-none"}`}>
       {list.map((item, index) =>
         item.heading ? (
           <p key={index} className="p-4 text-sm font-bold text-white uppercase leading-[17px]">
@@ -165,11 +226,19 @@ type ResourceItem = {
   children?: ResourceChild[];
 };
 
-const ResourceMenu = ({ list, isOpen = false }: { list: ResourceItem[]; isOpen?: boolean }) => {
+const ResourceMenu = ({
+  list,
+  isOpen = false,
+  isDesktop,
+}: {
+  list: ResourceItem[];
+  isOpen?: boolean;
+  isDesktop: boolean;
+}) => {
   return (
     <ul
-      className={`lg:block lg:absolute header-submenu bg-[#1B1B1B] rounded-[8px] lg:p-6 lg:w-max lg:right-auto transition-all lg:left-0 opacity-0 origin-top text-left top-auto scale-95 translate-z-0 group-hover:scale-100 group-hover:opacity-100 group-hover:left-auto group-hover:delay-150 group-hover:h-auto group-hover:overflow-visible
-     ${isOpen ? "scale-100 opacity-100 left-auto delay-150 h-auto overflow-visible" : ""}`}>
+      className={`lg:block lg:absolute header-submenu bg-[#1B1B1B] rounded-[8px] lg:p-6 lg:w-max lg:right-auto lg:transition-all lg:left-0 lg:opacity-0 origin-top text-left top-auto lg:scale-95 translate-z-0 group-hover:scale-100 group-hover:opacity-100 group-hover:left-auto group-hover:delay-150 w-full group-hover:overflow-visible lg:pointer-events-none lg:group-hover:pointer-events-auto overflow-hidden
+      ${isOpen ? "flex flex-col py-2 pointer-events-auto" : "hidden pointer-events-none"}`}>
       {list.map((item, index) =>
         item.heading ? (
           <p key={index} className="p-4 text-sm font-bold text-white uppercase leading-[17px]">
@@ -178,7 +247,9 @@ const ResourceMenu = ({ list, isOpen = false }: { list: ResourceItem[]; isOpen?:
         ) : (
           <li
             key={index}
-            className={`flex flex-col w-full ${item.children ? "group/blog relative" : ""}`}>
+            className={`flex flex-col w-full ${
+              isDesktop && item.children ? "group/blog relative" : ""
+            }`}>
             <a
               href={item.link}
               title={item.title}
@@ -221,13 +292,19 @@ const ResourceMenu = ({ list, isOpen = false }: { list: ResourceItem[]; isOpen?:
               )}
             </a>
             {item.children && (
-              <ul className="lg:absolute left-full -top-[23px] w-max bg-[#1B1B1B] rounded-r-[8px] opacity-0 pointer-events-none group-hover/blog:opacity-100 group-hover/blog:pointer-events-auto transition-opacity z-20 p-6">
+              <ul className="lg:absolute left-full -top-6 lg:w-max bg-[#1B1B1B] rounded-r-[8px] lg:opacity-0 lg:pointer-events-none group-hover/blog:opacity-100 group-hover/blog:pointer-events-auto transition-opacity z-20 lg:p-6 p-3 w-full">
                 {item.children.map((child, childIdx) => (
                   <li key={childIdx}>
                     <a
                       href={child.link}
-                      className="flex gap-2 px-4 py-2 text-sm text-secondary/80 hover:bg-header-bg hover:text-[#AFCAFF] rounded-[8px] transition-colors">
-                      <Image src={child.icon} alt="" width={24} height={24} className="mr-0.5" />
+                      className="flex gap-2 px-4 py-2 text-sm text-secondary/80 hover:bg-header-bg hover:text-[#AFCAFF] rounded-[8px] transition-colors items-center">
+                      <Image
+                        src={child.icon}
+                        alt=""
+                        width={24}
+                        height={24}
+                        className="mr-0.5 size-6"
+                      />
                       <div>
                         <p className="text-sm font-bold">{child.title}</p>
                         <p className="text-xs text-secondary/80">{child.subtitle}</p>
